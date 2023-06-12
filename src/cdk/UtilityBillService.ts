efe47f24c05c23b3d2fd9ab5908c6840fd9084e5
Angelo Yang <angryang@amazon.com>
2023-06-12 15:38:58 -0500

    Add rule and DLQ, change styling, and clean npm function

--- src/cdk/UtilityBillService.ts
+++ src/cdk/UtilityBillService.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { UtilityBillLambdaStack } from './UtilityBillLambda-Stack';
+import { UtilityBillLambdaStack } from './UtilityBillServiceLambda-Stack';
 
 
 
 const app = new cdk.App();
-new UtilityBillLambdaStack(app, 'UtilityBillLambdaStack', {
+
+new UtilityBillLambdaStack(app, 'UtilityBillServiceLambdaStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
---
+++ src/cdk/UtilityBillServiceLambda-Stack.ts
@@ -0,0 +1,75 @@
+
+import { Construct } from 'constructs';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { execSync } from 'child_process';
+import { IFunction } from "aws-cdk-lib/aws-lambda";
+import lambda = require('aws-cdk-lib/aws-lambda');
+import { IRule, Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+
+
+export class UtilityBillLambdaStack extends Stack {
+  public readonly lambdaFunction: IFunction;
+  public readonly scheduleRule: Rule;
+
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+
+    // --- lambda function
+    this.lambdaFunction = new lambda.Function(this, 'UtilityBillServiceLambda', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'main.handler',
+      code: lambda.Code.fromAsset('./src/lambda', {
+        bundling: {
+          image: lambda.Runtime.PYTHON_3_9.bundlingImage,
+          command: [],
+          local: {
+            tryBundle(outputDir: string) {
+              try {
+                execSync('pip3 --version');
+              } catch {
+                return false;
+              }
+
+              const commands = [
+                `cd src/lambda`,
+                `pip3 install -r requirements.txt -t ${outputDir}`,
+                `cp -a . ${outputDir}`
+              ];
+
+              execSync(commands.join(' && '));
+              return true;
+            }
+          }
+        }
+      }),
+      memorySize: 1024,
+      // functionName: 'UtilityBillLambda', // We do NOT specify a name so that replacements work.
+      timeout: Duration.seconds(300)
+    });
+
+
+    // --- eventbridge Rule for continual trigger
+    this.scheduleRule = new Rule(this, "UtilityBillServiceSchedulerRule", {
+      // ruleName: "UtilityBillServiceSchedulerRule",
+      description: "Runs the UtilityBillServiceLambda twice a month. On the 1st and 20th of every month, at 6pm",
+      schedule: Schedule.cron({
+        minute: '0',
+        hour: '18',
+        month: '*',
+        day:'1,20',
+        year: '*'
+      }),
+    });
+
+    // --- binding eventbridge rule to lambda and adding DLQ
+    this.scheduleRule.addTarget(new LambdaFunction(this.lambdaFunction, {
+      deadLetterQueue: new Queue(this, "UtilityBillServiceSchedulerDLQ"), // Optional: add a dead letter queue
+      retryAttempts: 2, // Optional: set the max number of retry attempts
+    }));
+
+  }
+}
